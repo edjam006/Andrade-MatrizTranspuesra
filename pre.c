ba27f6070e640dcb43f9038cab61d91afad252e8
Eduardo Andrade <eduardoandrade@MacBook-Air-de-Eduardo.local>
2023-11-27 17:05:27 -0500

    Librerias agregadas y solicitud de dimension de librerias

---
+++ pre.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    srand (time(NULL));
+
+    int a,b;
+    printf("Ingrese el numero de filas de la matriz\n");
+    scanf("%d", &a);
+    printf("Ingrese el numero de columnas de la matriz\n");
+    scanf("%d", &b);
+    int matriz[a][b];
+
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            matriz[i][j]= rand() % 101;
+        }
+        
+    }
+    printf("%d", matriz[a][b]);
+    
+    return 0;
+}
